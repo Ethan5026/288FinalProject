9e67a7c4f2dbf87ff46d534528560c7347e693e2
Denning <idenning@iastate.edu>
2023-11-27 12:09:49 -0600

    Entertainer implement

--- FinalProject/entertainer.h
+++ FinalProject/entertainer.h
@@ -8,15 +8,15 @@
 #ifndef ENTERTAINER_H_
 #define ENTERTAINER_H_
 
-//The volume of the speakers, don't know if it can be implemented
-int volume;
+#include "open_interface.h"
+
 /*
  * Set up the sound system for The Entertainer, the song played by the ice cream truck
  */
 void entertainer_init();
 
 //play the song
-void entertainer_play();
+void entertainer_play(oi_t* oi);
 
 //Set volume for the speakers. Have no idea if this is actually something you can do
 void entertainer_volume(int volume);
