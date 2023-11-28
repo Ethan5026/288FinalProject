a13fece8f8723e61ae7e05a70c6b7d6f709125d9
Ethan Gruening <114963766+Ethan5026@users.noreply.github.com>
2023-11-28 16:25:43 -0600

    Update calibrate.h

--- FinalProject/calibrate.h
+++ FinalProject/calibrate.h
@@ -8,18 +8,19 @@
 #ifndef CALIBRATE_H_
 #define CALIBRATE_H_
 
+void calibrate_IR(oi_t *sensor);
+
 //Calibrate the distance driven and give the distance error coefficient
 //ex. if the Cybot is told to move 100cm and moves 80 cm then the error coefficient is 1.25
-double calibrate_driveDistance(oi_t *sensor);
+void calibrate_driveDistance(oi_t *sensor);
 
 //Calibrate the angle driven and give the angle error coefficient
 //ex. if the Cybot is told to turn 100 degrees and moves 80 degrees then the error coefficient is 1.25
-double calibrate_driveDistance(oi_t *sensor);
+void calibrate_angle(oi_t *sensor);
 
 //Calculate the coefficient that needs to be multiplied by the timer values to get an accurate distance
-double calibrate_ping();
+void calibrate_ping();
 
-//Find the upper and lower bounds for the pwm
-void calibrate_pwm()
+void calibrate_everything(oi_t *sensor);
 
 #endif /* CALIBRATE_H_ */
