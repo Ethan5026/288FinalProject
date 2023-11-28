3c43a819ee6fc0a64e3790f9378f5242b3499c40
Ethan Gruening <114963766+Ethan5026@users.noreply.github.com>
2023-11-28 16:25:12 -0600

    Update calibrate.c

--- FinalProject/calibrate.c
+++ FinalProject/calibrate.c
@@ -5,74 +5,96 @@
  *      Author: ethgru
  */
 #include "IR.h"
+#include "uart.h"
+#include "movement.h"
+#include "ping.h"
+#include "button.h"
+#include "pwm.h"
 
 void calibrate_IR(oi_t *sensor){
     IR_init();
+    ping_init();
     //Continuously output the Ping sensor and IR values
     char message[60];
-
-    //move 2 meters and output values
-    int i;
-    double sumIR = 0;
-    double sumIR2 = 0;
-    double sumCm = 0;
-    double sumIRCm = 0;
     uart_sendStr("IR   PING");
 
-    //output and store all values
-    int count = 50;
-    for(i = 0; i < count; i++){
+    sprintf(message, "Ping\tIR");
+
+    while(1){
         double pingVal = ping_receive();
         int irVal = IR_receive();
-        sumIR += log(irVal);
-        sumIR2 += log(irVal) * log(irVal);
-        sumCm += log(pingVal);
-        sumIRCm += log(irVal) * log(pingVal);
-
-        sprintf(message, "%d %.2f\n\r", irVal, pingVal);
         uart_sendStr(message);
-        move_backward(sensor, 1);
+        sprintf(message, "%.3f\t%.3f\n", pingVal, irVal);
+        uart_sendStr(message);
+        lcd_printf(message);
         timer_waitMillis(500);
     }
-    double ex = (count * sumIRCm - sumIR * sumCm) / (count * sumIR2 - sumIR * sumIR);
-    double coff = exp((sumIR2 * sumCm - sumIRCm * sumIR) / (count * sumIR2 - sumIR * sumIR));
-    sprintf(message, "Exponent value: %.3f\nCoefficient Value: %.3f\n", ex, coff);
-    uart_sendStr(message);
-    lcd_printf(message);
+}
+void calibrate_driveDistance(oi_t *sensor){
+    distance_coef = 1;
+    move(sensor, 100);
+    lcd_printf("100/ActCM = distOff");
 }
 
-void calibrate_pwm(){
-    lcd_init();
+void calibrate_angle(oi_t *sensor){
+    angle_coef = 1;
+    turn(sensor, 90);
+    lcd_printf("360/ActAng = angOff");
+}
+void calibrate_ping(){
+    ping_coef = 1;
+    char message[50];
+    double pingVal;
+    while(1){
+        pingVal = ping_receive();
+        sprintf(message, "%.2lf", pingVal);
+        uart_sendStr(message);
+        lcd_printf("%.2lf",pingVal);
+    }
+}
+void calibrate_everything(oi_t *sensor){
+    pwm_init();
+    move_init();
     button_init();
-    int matchValue = 298238;
-    lcd_printf("Press Buttons 1 and 2 to\nposition at 0 degrees\nPress 4 to enter");
-    while(button_getButton() != 4){
-        if(button_getButton() == 2){
-            matchValue++;
-        }
-        else if(button_getButton() == 1){
-            matchValue--;
-        }
-        pwm_setMATCH(matchValue);
-        timer_waitMicros(200);
+    ping_init();
+    lcd_printf("Press buttons to move to next steps");
+    while(button_getButton() == 0){
 
     }
-    while(button_getButton() == 4){}
-    timer_waitMillis(20);
-    rightCalibration = matchValue;
-
-    lcd_printf("Press Buttons 1 and 2 to\nposition at 180 degrees\nPress 4 to enter");
-    while(button_getButton() != 4){
-        if(button_getButton() == 2){
-            matchValue++;
-        }
-        else if(button_getButton() == 1){
-            matchValue--;
-        }
-        pwm_setMATCH(matchValue);
-        timer_waitMicros(200);
+    timer_waitMillis(1000);
+    lcd_printf("PWM Calibration");
+    while(button_getButton() == 0){
 
     }
-    leftCalibration = matchValue;
-    lcd_printf("Right: %d\nLeft: %d", rightCalibration, leftCalibration);
+    timer_waitMillis(1000);
+    pwm_config();
+    pwm_setAngle(90);
+    timer_waitMillis(1000);
+    while(button_getButton() == 0){
+
+    }
+    timer_waitMillis(1000);
+
+    lcd_printf("Calibrate drive\nDriving 1m\nMeasure Actual");
+    while(button_getButton() == 0){
+
+    }
+    timer_waitMillis(1000);
+    calibrate_driveDistance(sensor);
+    while(button_getButton() == 0){
+
+    }
+    timer_waitMillis(1000);
+    lcd_printf("Calibrate angle\nTurning 360\nMeasureActual");
+    while(button_getButton() == 0){
+
+    }
+    timer_waitMillis(1000);
+    calibrate_angle(sensor);
+    while(button_getButton() == 0){
+
+    }
+    timer_waitMillis(1000);
+    lcd_printf("Calibration Complete");
 }
+
